93b64ba40db48ee157fe70d849f668b9b9b0a8b4
Oliverio Rojas Sanchez <158488775+22620188Oliverio@users.noreply.github.com>
2025-05-07 13:59:44 -0600

    first commit

---
+++ gym/src/main/java/com/koj/gym/GymApplication.java
@@ -0,0 +1,13 @@
+package com.koj.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymApplication.class, args);
+	}
+
+}
---
+++ gym/src/test/java/com/koj/gym/GymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koj.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
