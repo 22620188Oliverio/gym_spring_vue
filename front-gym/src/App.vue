db7d11ae684a3b2b07efa37da8f8db42b368ffb5
KevCH24 <22620023@tlaxiaco.tecnm.mx>
2025-05-14 14:50:11 -0600

    Act13

--- front-gym/src/App.vue
+++ front-gym/src/App.vue
@@ -14,8 +14,39 @@ onMounted(cargargym);
 
 <template>
 <div class="container">
-  <button class="btn btn-primary">boton </button>
-  </div>
+<h2>Tabla de clientes</h2>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Número membresia</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Apellidos</th>
+      <th scope="col">Telefono</th>
+      <th scope="col">Email</th>
+      <th scope="col">Imagen URL</th>
+      <th scope="col">Tipo de membresia</th>
+      <th scope="col">Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(cliente, index) in gym" :key="index">
+      <td>{{ cliente.id }}</td>
+      <td>{{ cliente.numeroMembresia }}</td>
+      <td>{{ cliente.nombre }}</td>
+      <td>{{ cliente.apellidos }}</td>
+      <td>{{ cliente.telefono }}</td>
+      <td>{{ cliente.email }}</td>
+      <td>{{ cliente.imagenURL }}</td>
+      <td>{{ cliente.tipoMembresia }}</td>
+      <td>
+        <button class="btn btn-primary">Editar</button>
+        <button class="btn btn-danger">Eliminar</button>
+      </td>
+    </tr>
+  </tbody>
+</table> 
+</div>
 
 </template>
 
